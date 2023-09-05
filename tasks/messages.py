d5adc5e1152670e8cee09a248c961e38c12aa0da
dearsecret <dearsecret1217@gmail.com>
2023-09-05 16:41:35 +0900

    sms bytes config

--- tasks/messages.py
+++ tasks/messages.py
@@ -3,8 +3,10 @@ from .signature import sms
 
 
 def send_sms(to: str, content: str):
-    try:
-        res = requests.post(**sms(to, content))
-        res.raise_for_status()
-    except Exception as e:
-        print(e)
+    # SMS bytes 크기 90으로 제한된다.
+    if len(content.encode()) < 90:
+        try:
+            res = requests.post(**sms(to, content))
+            res.raise_for_status()
+        except Exception as e:
+            print(e)
